991086ca5fd902d5cf5d60a9e29ad42e648a2960
Ana Julia Velez Rueda <anavelezrueda@gmail.com>
2021-01-03 02:13:25 -0300

    Enable helmet, authentication and rate middlewares

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express");
-//const rateLimit = require("express-rate-limit");
-const routes = require("./routes/index.js");
-const app = express();
 
-//const limiter = rateLimit({
-//windowMs: 60 * 60 * 1000,
-//    max: 5
-//});
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const authentication = require("./routes/authentication.js");
+
+const routes = require("./routes/index.js");
+
+const app = express();
 
 if (!process.env.ACCESS_TOKEN_SECRET) {
     throw new Error("Please set ACCESS_TOKEN_SECRET before running app");
 }
 
+app.use(helmet());
+app.use(rateLimit({windowMs: 60 * 60 * 1000, max: 5}));
+app.use(authentication);
+app.use(express.json());
+
+routes(app);
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Api escuchando el puerto: 3000`);
 });
-app.use(express.json());
-
-//app.use(limiter)
-
-routes(app);
 
 module.exports = app;
