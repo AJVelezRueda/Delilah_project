43dfc09dfd47eaed327016c491056e705360a0fd
Ana Julia Velez Rueda <anavelezrueda@gmail.com>
2021-01-03 02:06:13 -0300

    Parameterize DB name

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(express.json());
 
 //app.use(limiter)
 
-
-
 routes(app);
 
 module.exports = app;
--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('test', 'delilah', 'D3L1L4HIS@W3S0m3!i5N\'T1t?', {
+const db = new Sequelize(process.env.DB_NAME, 'delilah', 'D3L1L4HIS@W3S0m3!i5N\'T1t?', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql'
